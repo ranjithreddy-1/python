91767c4cd2be18a1e974826ce2ba1a92a4c831a6
ranjithreddy-1 <ranjithreddyranjithreddy41@gmail.com>
2026-07-07 20:07:21 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,78 @@
+'''a=2
+b=4
+print(a+b)'''
+
+'''a=6
+b=8
+c=a+b
+print(c)'''
+
+#print(10+10)
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter b value")
+print(a+b)'''
+
+'''a=float(input("enter a value")
+b=float(input("enter b value")
+print(a+b)'''
+
+'''a=str(input("data1")
+b=str(input("data2")
+print(a+b)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter a value")
+b=complex(input("enter b value")
+print(a+b)'''
+
+'''a=bool(input("enter a value"))
+b=bool(input("enter b value"))
+c=bool(input("enter c value"))
+print(a+b+c)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+#swapping of two variables
+'''a=10
+b=20
+a,b=b,a
+print(a,b)'''
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))
+                 
+       
+
