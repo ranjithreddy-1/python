9afc8d145bf11af6409be5a092f9dcd06388444e
ranjithreddy-1 <ranjithreddyranjithreddy41@gmail.com>
2026-07-12 18:53:25 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,107 @@
+#voting eligiblity
+'''age=int(input("enter age: "))
+if age>=18:
+        print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+#to check given number is even or odd
+'''num=int(input("enter num:"))
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")'''
+#to check given number is leap year or non leap year
+'''year=int(input("enter the year: "))
+if year%4==0:
+    print("it is leap year")
+else:
+    print("it is not leap year")'''
+
+#guest code
+'''name=input("enter name: ")
+if name=="ranjith":
+    print("welcome ranjith")
+else:
+    print("welcome guest")'''
+
+#multiple user
+'''names=["ramu","remo","raj","tony","jerry"]
+a=input("enter name: ").lower()
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+#vowels
+'''vowels=["a","e","i","o","u"]
+a=input("enter letter: ")
+if a in vowels:
+    print(" it is vowel")
+else:
+    print("it is consonant")'''
+
+#social media login using nested if
+
+'''username=input("enter username: ")
+password=int(input("enter password: "))
+if username=="ranjith":
+     if password=="1234":
+          print("login successfully")
+     else:
+         print("incorrect password")
+else:
+    print("invalid credentials")'''
+
+'''username=input("enter username: ")
+password=int(input("enter password: "))
+if username=="ranjith" and password==1234:
+    print("login successfully")
+else:
+    print("invalid credentials")'''
+
+#multiple if
+'''age=int(input("enter age: "))
+marks=int(input("enter marks: "))
+attendence=int(input("enter attendence: "))
+if age>=18:
+    print("eligible to vote")
+if marks>=80:
+    print("eligible for scholarship")
+if attendence>=75:
+    print("eligible to write exams")'''
+
+#bakery
+'''a=int(input("enter the cake price: "))
+if a==1200:
+    print("redvelvet cake")
+elif a==1000:
+    print("almond cake")
+elif a==800:
+    print("choclate cake")
+elif a==600:
+    print("butterscotch")
+else:
+    print("sorry cake is not available")'''
+
+#pizza
+'''a=input("enter the pizza name")
+if a=="bbq pizza":
+    print(1000)
+elif a=="chicken pizza":
+    print(800)
+elif a=="paneer pizza":
+    print(600)
+elif a=="corn pizza":
+    print(600)'''
+    
+
+
+
+
+
+
+
+    
+
+
+    
